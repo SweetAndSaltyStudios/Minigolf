0f58db09f7644f0b70bc96c2d89dbe540ae617f8
Mathias Mustonen <mathias.mustonen@gmail.com>
2020-08-08 22:12:07 +0300

    Project cleaning

---
+++ Assets/Scripts/BallEngine.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallEngine : MonoBehaviour
+{
+    [SerializeField] private Color32 ballColor = default;
+
+    private Rigidbody rb = default;
+
+    private MeshRenderer meshRenderer = default;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        meshRenderer = GetComponentInChildren<MeshRenderer>();
+    }
+
+    private void Start()
+    {
+        if(meshRenderer == null) return;
+
+        meshRenderer.material.color = ballColor;
+    }
+
+    public void Shoot(float dragDistance)
+    {
+        if(rb == null) return;
+
+        rb.AddForce(Vector3.forward * dragDistance, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+namespace Sweet_And_Salty_Studios.Inputs
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] private BallEngine ballEngine = default;
+        [SerializeField] private LineRenderer lineRenderer = default;
+        [SerializeField] private Camera mainCamera = default;
+
+        private Vector2 pointerStartDragPosition = default;
+        private Vector2 pointerEndDragPosition = default;
+
+        private float dragDistance = default;
+
+        private void Update()
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                pointerStartDragPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+                lineRenderer.SetPosition(0, new Vector3(pointerStartDragPosition.x, 0, pointerStartDragPosition.y));
+            }
+
+            if(Input.GetMouseButton(0))
+            {
+                pointerEndDragPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+                dragDistance = (pointerEndDragPosition - pointerStartDragPosition).magnitude;
+
+                lineRenderer.SetPosition(1, new Vector3(pointerEndDragPosition.x, 0, pointerEndDragPosition.y));
+            }
+
+            if(Input.GetMouseButtonUp(0))
+            {
+                if(ballEngine != null)
+                {
+                    ballEngine.Shoot(dragDistance);
+                }
+
+                pointerStartDragPosition = default;
+                pointerEndDragPosition = default;
+            }
+        }
+    }
+}
